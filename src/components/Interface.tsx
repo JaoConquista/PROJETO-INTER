41498476c77a586cbc5442aac87acaa5a6ec5bd0
João Conquista <conquistajoaovictor@gmail.com>
2023-04-28 14:19:35 -0300

    Interfatualizando o front

--- src/components/Interface.tsx
+++ src/components/Interface.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 import { AiOutlineSearch } from "react-icons/ai";
+import { MdAccountCircle } from "react-icons/md";
 //CSS
 import styles from '../ModuleCss/Interface.module.css'
 
@@ -10,25 +11,49 @@ import styles from '../ModuleCss/Interface.module.css'
 const Interface = () => {
   return (
     <div className={styles['main']}>
-        <div id={styles['side-bar']}>
+
+        <div className="section1">
+          <div id={styles['side-bar']}>
 
           <button className={styles['content-side-bar']}><Link to='/principal'>Início</Link></button>
-          
+
           <button className={styles['content-side-bar']}><Link to='/vendas'>Vendas</Link></button>
-          
+
           <button className={styles['content-side-bar']}><Link to='/produtos'>Produtos</Link></button>
-          
+
           <button className={styles['content-side-bar']}><Link to='/estoque'>Estoque</Link></button>
+          </div>
         </div>
-        <div id={styles['result']}>
-          <input type="text" 
-          placeholder='pesquise algum produto'
-          />
-          <button id={styles['icons']}><AiOutlineSearch/></button>
-          
-        </div>
-        <div id={styles["interface"]}>
+        <div className="section2">
+          <div className={styles['account']}>
+            <p>João Conquista</p> <MdAccountCircle/>
+          </div>
+          <div id={styles["interface"]}>
           <h1>Principal</h1>
+          <div>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dignissimos ea quod commodi a. Veniam, voluptatum. Maiores sit, eos laboriosam, quidem corporis excepturi eius aspernatur ut dolorem, accusantium architecto rerum?</h3>
+          </div>
+          </div>
         </div>
         
         
--- src/components/InterfaceVendas.tsx
+++ src/components/InterfaceVendas.tsx
@@ -9,28 +9,30 @@ import styles from '../ModuleCss/Interface.module.css'
 const InterfaceVendas = () => {
     return (
         <div className={styles['main']}>
-            <div id={styles['side-bar']}>
+            <div className="section1">
+        <div id={styles['side-bar']}>
 
-            <button className={styles['content-side-bar']}><Link to='/principal'>Início</Link></button>
-              
-            <button className={styles['content-side-bar']}><Link to='/vendas'>Vendas</Link></button>
+          <button className={styles['content-side-bar']}><Link to='/principal'>Início</Link></button>
+
+          <button className={styles['content-side-bar']}><Link to='/vendas'>Vendas</Link></button>
+
+          <button className={styles['content-side-bar']}><Link to='/produtos'>Produtos</Link></button>
+
+          <button className={styles['content-side-bar']}><Link to='/estoque'>Estoque</Link></button>
+        </div>
+            </div>
+            <div className="section2">
+        <div id={styles['result']}>
+          <input type="text" 
+          placeholder='pesquise algum produto'
+          />
+          <button id={styles['icons']}><AiOutlineSearch/></button>
           
-            <button className={styles['content-side-bar']}><Link to='/produtos'>Produtos</Link></button>
-          
-            <button className={styles['content-side-bar']}><Link to='/estoque'>Estoque</Link></button>
-            </div>
-            <div id={styles['result']}>
-              <input type="text" 
-              placeholder='pesquise algum produto'
-              />
-              <button id={styles['icons']}><AiOutlineSearch/></button>
-              
-            </div>
-            <div id={styles["interface"]}>
-                <h1>Vendas</h1>
-            </div>
-            
-            
+        </div>
+        <div id={styles["interface"]}>
+          <h1>Vendas</h1>
+        </div>
+            </div>            
         </div>
       )
 }
