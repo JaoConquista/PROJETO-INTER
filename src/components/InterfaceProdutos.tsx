b55b464dd1671a1fda076dd7ff1f0a5826966b53
João Conquista <conquistajoaovictor@gmail.com>
2023-04-29 12:11:31 -0300

    cards de produtos adicionados

--- src/components/InterfaceProdutos.tsx
+++ src/components/InterfaceProdutos.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import {Link} from 'react-router-dom'
 
-import { AiOutlineSearch } from "react-icons/ai";
+import { AiOutlineSearch , AiFillEdit} from "react-icons/ai";
 //CSS
 import styles from '../ModuleCss/Interface.module.css'
-
+import styleProdutos from '../ModuleCss/InterfaceProdutos.module.css'
 const InterfaceProdutos = () => {
     return (
         <div className={styles['main']}>
@@ -21,18 +21,42 @@ const InterfaceProdutos = () => {
           <button className={styles['content-side-bar']}><Link to='/estoque'>Estoque</Link></button>
         </div>
             </div>
-            <div className="section2">
-        <div id={styles['result']}>
-          <input type="text" 
-          placeholder='pesquise algum produto'
-          />
-          <button id={styles['icons']}><AiOutlineSearch/></button>
-          
-        </div>
-        <div id={styles["interface"]}>
-          <h1>Produtos</h1>
-        </div>
-            </div>  
+        <div className="section2">
+          {/*<div id={styles['result']}>
+            <input type="text"
+              placeholder='pesquise algum produto'
+            />
+            <button id={styles['icons']}><AiOutlineSearch /></button>
+          </div>*/}
+          <div id={styles["interface"]}>
+            <div id="title"><h1>Produtos</h1></div>
+            <div id={styleProdutos["content-cards"]}>
+              <div className={styleProdutos["card-produtos"]}>
+                  <div className={styleProdutos["section1-card"]}>
+                    <span>
+                      <p className={styleProdutos['nome']}>Nome</p>
+                      <p>Vinho Branco</p>
+                    </span>
+                    <span>
+                      <p className={styleProdutos['marca']}>Marca</p>
+                      <p>Italiano</p>
+                    </span>
+                  </div>
+                  <div className={styleProdutos["section2-card"]}>
+                    <div className={styleProdutos["informations"]}>
+                      <span><p>Unidades</p><p>20 </p></span>
+                      <span><p>P / Custo</p><p>80,00 R$</p></span>
+                      <span><p>P / Venda</p><p>300,00 R$</p></span>
+                      <span><p>Validade</p><p>20/05/2029</p></span>
+                    </div>
+                    <div className={styleProdutos["icons"]}>
+                      <AiFillEdit/>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>  
         </div>
       )
 }
